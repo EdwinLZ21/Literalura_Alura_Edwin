6525c99301632da524417d1d09ed362d793196bc
Edwin Lopez <edwin21899@gmail.com>
2025-07-20 14:07:43 +0200

    Primer commit - proyecto Literalura

---
+++ src/main/java/com/alura/Literalura_Alura_Edwin/LiteraluraAluraEdwinApplication.java
@@ -0,0 +1,13 @@
+package com.alura.Literalura_Alura_Edwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraAluraEdwinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraAluraEdwinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/Literalura_Alura_Edwin/LiteraluraAluraEdwinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.Literalura_Alura_Edwin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraAluraEdwinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
